3b2f62b28ddaab7a0189767c23976fcd565ba2c7
Si_Jiu49 <si_jiu49@outlook.com>
2025-04-10 21:50:18 +0800

    added banner credit

--- src/config.ts
+++ src/config.ts
@@ -19,9 +19,9 @@ export const siteConfig: SiteConfig = {
     src: 'assets/images/banner.jpg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
-      enable: false,         // Display the credit text of the banner image
-      text: '',              // Credit text to be displayed
-      url: ''                // (Optional) URL link to the original artwork or artist's page
+      enable: true,         // Display the credit text of the banner image
+      text: '中华人民共和国政府',              // Credit text to be displayed
+      url: 'https://www.gov.cn/'                // (Optional) URL link to the original artwork or artist's page
     }
   },
   toc: {
