c176d9ba07c7a31b9deb592a7b52f37644c5e84a
肆鸠 <si_jiu49@outlook.com>
2025-05-15 10:31:55 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://blog.sijiu49.us.kg/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
