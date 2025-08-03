ab4b75228b29c9d48d28b3b267286a6f9e9945a1
Si_Jiu49 <si_jiu49@outlook.com>
2025-08-03 11:02:53 +0800

    修改了bio

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "肆鸠",
-	bio: "不知道",
+	bio: "我个人觉得呢，这个意大利面就应该拌42号混凝土",
 	links: [
 		//{
 		//	name: "Twitter",
