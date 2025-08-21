e83f674cb9e6522a6458837e6b1c081efa029640
Si-Jiu <si_jiu49@outlook.com>
2025-08-21 15:08:54 +0800

    修改配置

--- src/config.ts
+++ src/config.ts
@@ -107,8 +107,9 @@ export const siteConfig: SiteConfig = {
 
 export const navBarConfig: NavBarConfig = {
 	links: [
-		LinkPreset.Home, 
+		LinkPreset.Home,
 		LinkPreset.Archive,
+		LinkPreset.About
 		// 支持自定义导航栏链接,并且支持多级菜单,3.1版本新加
 		// {
 		// 	name: "Links",
