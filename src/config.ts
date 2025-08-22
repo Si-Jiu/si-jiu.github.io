fb33a517bbf1ce2e4ecc6ce6a67990ea87e29e93
Si-Jiu <si_jiu49@outlook.com>
2025-08-22 20:35:21 +0800

    fix umami

--- src/config.ts
+++ src/config.ts
@@ -161,7 +161,7 @@ export const profileConfig: ProfileConfig = {
 	],
 	// Umami统计部份，记得在layout插入Umami的head标签
 	umami: {
-		enable: false, // 是否显示umami统计
+		enable: true, // 是否显示umami统计
 		shareId: "G65zrdEkxpFVGuEf", //填入共享URL最后面那一串  比如：https://eu.umami.is/api/share/2dKQ5T0WrUn6AYtr 你就填入2dKQ5T0WrUn6AYtr
 		region: "us", //Umami有两个区域，按需选择即可  比如：https://eu.umami.is 你就填入eu
 	},
